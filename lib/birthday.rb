f52ca2b2bb17166c264007360e1057b9897586d9
sydney miranda <sydmiranda@icloud.com>
2021-01-04 23:43:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,5 +6,6 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each do |n
+  birthday_kids.each do | name, age |
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
 end
